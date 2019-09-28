a99a186c390c20575b7e25fe0c043534ec81c88a
kostya05983 <kostya05983@mail.ru>
2019-09-29 01:45:58 +0700

    Add listView component

--- src/jsMain/kotlin/ru/jetspirit/components/ListView.kt
+++ src/jsMain/kotlin/ru/jetspirit/components/ListView.kt
@@ -4,14 +4,19 @@ import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import styled.css
 import styled.styledDiv
 
-class Header: RComponent<RProps, RState>() {
+/**
+ * @author Konstantin Volivach
+ */
+class ListView : RComponent<ListViewProps, RState>() {
     override fun RBuilder.render() {
         styledDiv {
-            css {
-            }
+            props.items.map { it.render() }
         }
     }
+}
+
+interface ListViewProps : RProps {
+    var items: List<RComponent<*, *>>
 }
